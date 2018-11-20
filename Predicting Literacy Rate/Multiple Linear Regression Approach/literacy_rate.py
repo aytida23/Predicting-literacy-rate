c491852a262cdbd83bfe080026a303c15dcc84ff
Aditya Mandal <aditya.cse.nitap@gmail.com>
2018-11-20 23:18:00 +0530

    Imported libraries and the dataset to get started with the prediction

---
+++ Predicting Literacy Rate/Multiple Linear Regression Approach/literacy_rate.py
@@ -0,0 +1,14 @@
+"""
+This is a try to predict the literacy rate of countries.
+"""
+# Literacy rate Prediction
+
+# Multiple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('countries_of_the_world.csv', decimal = ',')
